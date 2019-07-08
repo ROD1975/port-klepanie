7774c087b6d822f9fedc5506ba270212dcb2aafc
unknown <konradb1975@gmail.com>
2019-07-08 19:56:58 +0200

    for

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,25 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 // const links = document.querySelectorAll('.nav-links li');
-const navItem = document.querySelectorAll('.nav-item');
+// const navItem = document.querySelectorAll('.nav-item');
 
+// navItem.forEach((navItem) => {
+//     navItem.addEventListener('click', () => {
+//       navLinks.classList.toggle('open');
+//     });
+//   });
 
+var navItems = document.querySelectorAll('.nav-item');
 
-navItem.forEach((navItem) => {
-    navItem.addEventListener('click', () => {
+for (i = 0; i < navItems.length; ++i) {
+    navItems[i].addEventListener('click', () => {
         navLinks.classList.toggle('open');
-    });
-  });
+    })
+};
 
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('open');
 })
-
 
 
